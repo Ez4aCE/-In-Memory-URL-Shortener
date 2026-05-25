524ed5e52a442642a02b1d4484502ae5c9eac868
Ez4aCE <himanshu24rai@gmail.com>
2026-05-25 15:43:42 +0530

    feat: add health check endpoint

--- cmd/server/main..go
+++ cmd/server/main..go
@@ -1,1 +1,21 @@
-package server
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "ok")
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", healthHandler)
+	fmt.Println("Listening on port 8080")
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		fmt.Println("Server Error", err)
+	}
+
+}
